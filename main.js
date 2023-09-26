ec2afa2c3779990eef1c73bdb3b658e8cd198cfd
Mayeul <50779795+123Leuyam456@users.noreply.github.com>
2023-09-26 13:02:35 +0200

    Exercice 3

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
-var a = 4;
-var b = 4;
-var c = 3;
- 
-switch(a){
-    case (b):
-        console.log('égal à b');
-        break;
-    case (c):
-        console.log('égal à c');
-        break;
-    default :
-        console.log('égal à rien');
+var a = 2000;
+for (let i= 0; i<a ; i++){
+    console.log("oklm");
 }

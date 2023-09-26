7c7958abe84c1b43233f73d5bc153fbac69bf41b
Mayeul <50779795+123Leuyam456@users.noreply.github.com>
2023-09-26 13:02:54 +0200

    Exercice 4

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-var a = 2000;
-for (let i= 0; i<a ; i++){
-    console.log("oklm");
+let a = 3
+
+while (a <9){
+    a++
+    console.log(a);
+    if (a == 8){
+        break;
+    }
+    else if (a == 7)
+    {
+        continue;
+    }
 }

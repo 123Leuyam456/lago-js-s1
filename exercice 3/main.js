a4fe7db8d05ad3f88bbebcb96e6fc0ccf525543f
Mayeul <50779795+123Leuyam456@users.noreply.github.com>
2023-09-27 08:04:39 +0200

    Add files via upload

---
+++ exercice 3/main.js
@@ -0,0 +1,4 @@
+var a = 2000;
+for (let i= 0; i<a ; i++){
+    console.log("oklm");
+}
---
+++ exercice 4/main.js
@@ -0,0 +1,13 @@
+let a = 3
+
+while (a <9){
+    a++
+    console.log(a);
+    if (a == 8){
+        break;
+    }
+    else if (a == 7)
+    {
+        continue;
+    }
+}

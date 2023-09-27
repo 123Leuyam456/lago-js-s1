6595e6c798ee49df24ed8af5e755d9d6f81f30f1
Mayeul <50779795+123Leuyam456@users.noreply.github.com>
2023-09-27 13:01:25 +0200

    Add files via upload

---
+++ exercice 7/main.js
@@ -0,0 +1,9 @@
+var Mayeul =
+{
+    firstName : "Mayeul",
+    lastName : "Buiret",
+    age : 18,
+}   
+
+let humilite = "Voici " + Mayeul.firstName + " une des 7 merveilles du monde moderne, il porte le beau nom de  famille de " + Mayeul.lastName + ". Agé de " + Mayeul.age + " sa beauté transcende toute personne l'approchant."
+console.log(humilite)
